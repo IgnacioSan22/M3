3797499ac6a9f1e02949d84d9667e5c54f7c119a
IgnacioSan22 <iceamanos1998@gmail.com>
2022-01-20 08:52:20 +0100

    improve

--- trainings.py
+++ trainings.py
@@ -38,25 +38,8 @@ net_configurations = [
       ['Dense', 512, 'tanh'],
       ['Dense', 8, 'softmax']],
 
-#1-layer MLP with swish activation
-      [['Reshape', 32, 32],
-      ['Dense', 2048, 'swish'],
-      ['Dense', 8, 'softmax']],
 
-#2-layer MLP with swish activation
-      [['Reshape', 32, 32],
-      ['Dense', 2048, 'swish'],
-      ['Dense', 1024, 'swish'],
-      ['Dense', 8, 'softmax']],
-
-#3-layer MLP with swish activation
-      [['Reshape', 32, 32],
-      ['Dense', 2048, 'swish'],
-      ['Dense', 1024, 'swish'],
-      ['Dense', 512, 'swish'],
-      ['Dense', 8, 'softmax']],
-
-#4-layer MLP with relu and linear end activation
+# #4-layer MLP with relu and linear end activation
       [['Reshape', 32, 32],
       ['Dense', 2048, 'relu'],
       ['Dense', 1024, 'relu'],
@@ -65,7 +48,7 @@ net_configurations = [
       ['Dense', 8, 'softmax']],
 
 
-#4-layer MLP with relu and linear end activation
+# #4-layer MLP with relu and linear end activation
       [['Reshape', 32, 32],
       ['Dense', 2048, 'relu'],
       ['Dense', 1024, 'relu'],
@@ -73,4 +56,4 @@ net_configurations = [
       ['Dense', 256, 'sigmoid'],
       ['Dense', 8, 'softmax']]
     ]
-test_name = ['Relu-2', 'Relu-3', 'Relu-4', 'Tanh-2', 'Tanh-3', 'Tanh-4', 'Swish-2', 'Swish-3', 'Swish-4', 'ReluLin-5', 'ReluSig-5']
+test_name = ['Relu-2', 'Relu-3', 'Relu-4', 'Tanh-2', 'Tanh-3', 'Tanh-4', 'ReluLin-5', 'ReluSig-5']
