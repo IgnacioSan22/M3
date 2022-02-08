5eebb56d518d024fec28307b5d862c941deb50ca
IgnacioSan22 <iceamanos1998@gmail.com>
2022-02-08 12:55:59 +0100

    baseline dropout

--- W5/baseline.py
+++ W5/baseline.py
@@ -10,15 +10,13 @@ def make_model(input_shape):
 
     # Entry block
     x = layers.Conv2D(64,7,activation='relu',padding='same')(inputs)
-    # x = layers.Dropout(0.2)(x)
+    x = layers.Dropout(0.2)(x)
 
     x = layers.MaxPooling2D(2)(x)
     x = layers.Conv2D(128,3,activation='relu', padding='same')(x)
-    # x = layers.Dropout(0.2)(x)
-    x = layers.Conv2D(128,3,activation='relu', padding='same')(x)
+    x = layers.Dropout(0.2)(x)
 
     x = layers.MaxPooling2D(2)(x)
-    x = layers.Conv2D(256,3,activation='relu', padding='same')(x)
     x = layers.Conv2D(256,3,activation='relu', padding='same')(x)
 
     x = layers.AveragePooling2D(2)(x)
